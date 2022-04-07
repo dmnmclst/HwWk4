93f7deb758e795b1ba90379685651d8eb18e2c79
Chingiz Rysakov <chingizrysakov@MacBook-Pro-Chingiz.local>
2022-04-07 21:53:10 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,113 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+    public static int bossHealth = 700;
+    public static int bossDamage = 50;
+    public static String bossDefenceType;
+    public static int[] heroesHealth = {280, 270, 250, 300};
+    public static int[] heroesDamage = {10, 15, 20, 0};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medical"};
+    public static int round_number = 0;
+
+
+    public static void main(String[] args) {
+        printStatistics();
+        while (!isGameFinished()) {
+            round();
+        }
+    }
+
+
+    public static void chooseDefence() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(heroesAttackType.length); // 0, 1, 2
+        bossDefenceType = heroesAttackType[randomIndex];
+    }
+
+    public static void round() {
+        round_number++;
+        chooseDefence();
+        bossHits();
+        heroesHit();
+        printStatistics();
+    }
+
+    public static void bossHits() {
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0 && bossHealth > 0) {
+                if (heroesHealth[i] - bossDamage < 0) {
+                    heroesHealth[i] = 0;
+                } else {
+                    heroesHealth[i] = heroesHealth[i] - bossDamage; // heroesHealth[i] -= bossDamage;
+                }
+            }
+        }
+    }
+
+    public static void heroesHit() {
+        for (int i = 0; i < heroesDamage.length; i++) {
+            if (heroesHealth[i] > 0 && bossHealth > 0) {
+                if (bossDefenceType == heroesAttackType[i]) {
+                    Random random = new Random();
+                    int coeff = random.nextInt(12) + 2; // 2,3,4,5,6,7,8,9,10
+                    if (bossHealth - heroesDamage[i] * coeff < 0) {
+                        bossHealth = 0;
+                    } else {
+                        bossHealth = bossHealth - heroesDamage[i] * coeff;
+                    }
+                    System.out.println("Critical damage: " + heroesDamage[i] * coeff);
+                } else {
+                    if (bossHealth - heroesDamage[i] < 0) {
+                        bossHealth = 0;
+                    } else {
+                        bossHealth = bossHealth - heroesDamage[i];
+                    }
+                }
+            }
+        }
+    }
+
+    public static void printStatistics() {
+        System.out.println(round_number + " ROUND --------------------");
+        System.out.println("Boss health: " + bossHealth + " (" + bossDamage + "); " + bossDefenceType);
+        for (int i = 0; i < heroesHealth.length; i++) {
+            System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i]
+                    + " (" + heroesDamage[i] + ")");
+        }
+    }
+
+    public static void Medical() {
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] < 100 && heroesHealth[i] > 0) {
+                if (heroesHealth[3] < 100 && heroesHealth[3] > 0) {
+                    heroesHealth[3] = heroesHealth[3] - bossDamage;
+                } else {
+                    Random random = new Random();
+                    int coef = random.nextInt(10);
+                    heroesHealth[i] = bossDamage - (heroesHealth[i] + coef);
+                }
+            }
+        }
+    }
+
+    public static boolean isGameFinished() {
+        if (bossHealth <= 0) {
+            System.out.println("Heroes won!!!");
+            return true;
+        }
+        
+        boolean allHeroesDead = true;
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+        if (allHeroesDead) {
+            System.out.println("Boss won!!!");
+        }
+        return allHeroesDead;
+    }
+}
